0ec4a0a4fbe25cad35e3498fcb59d39383c36e09
Roman Gorev <ogogorev@yandex.ru>
2020-12-11 20:14:46 +0100

    Add basic login form layout

---
+++ src/components/login/login.js
@@ -0,0 +1,23 @@
+import { h } from 'preact';
+
+const Login = () => {
+  return (
+    <div>
+      <form action="">
+        <div>
+          <input type="text" />
+        </div>
+
+        <div>
+          <input type="text" />
+        </div>
+
+        <button type="submit">
+          login
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
