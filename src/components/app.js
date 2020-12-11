c1181320412cfbc5f0aa62f554a68301a1e20d34
Roman Gorev <ogogorev@yandex.ru>
2020-12-11 20:16:44 +0100

    Display login component

--- src/components/app.js
+++ src/components/app.js
@@ -1,20 +1,10 @@
 import { h } from 'preact';
-import { Router } from 'preact-router';
 
-import Header from './header';
-
-// Code-splitting is automated for `routes` directory
-import Home from '../routes/home';
-import Profile from '../routes/profile';
+import Login from './login/login';
 
 const App = () => (
 	<div id="app">
-		<Header />
-		<Router>
-			<Home path="/" />
-			<Profile path="/profile/" user="me" />
-			<Profile path="/profile/:user" />
-		</Router>
+		<Login />
 	</div>
 )
 
